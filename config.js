f6b3435f7753baf52b59fdbe20b3f52ada55e865
MARK WAMBIA <wambiamark45@gmail.com>
2025-05-21 10:52:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const conf = {
   AUTO_READ_DM: process.env.AUTO_READ_DM || "off",
   AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || "off",
   ADM: process.env.ANTI_DELETE || "on",
-  ON: process.env.OWNER_NAME || "FLASH-MD",
+  ON: process.env.OWNER_NAME || "PROFESSOR",
   ANTICALL: process.env.ANTICALL || "off",
   ANTIPROMOTE: process.env.ANTIPROMOTE || "off",
   ANTIDEMOTE: process.env.ANTIDEMOTE || "off",
@@ -15,14 +15,14 @@ const conf = {
   ANTILINK_ACTION: process.env.ANTILINK_ACTION || "delete",
   timezone: process.env.TIME_ZONE || "Africa/Nairobi",
   PRESENCE_DM: process.env.DM_PRESENCE || "typing",
-  PRESENCE_GROUP: process.env.GROUP_PRESENCE || "paused",
-  MODE: process.env.MODE || "private",
+  PRESENCE_GROUP: process.env.GROUP_PRESENCE || "recording",
+  MODE: process.env.MODE || "public",
   AV: process.env.AV || "on",
   PREFIXES: process.env.PREFIX !== undefined 
   ? process.env.PREFIX.split(",").map(p => p.trim()) 
   : [""], 
   Session: process.env.SESSION || "",
-  NUMBER: process.env.OWNER_NUMBER || "",
+  NUMBER: process.env.OWNER_NUMBER || "254112658916",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || ""
 };
